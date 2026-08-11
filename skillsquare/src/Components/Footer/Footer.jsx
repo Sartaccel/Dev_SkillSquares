0473b782fb559ff5e0be75e182c17abe68dd37a3
Harish_K <158758176+HARISHKALAIK@users.noreply.github.com>
2026-08-11 14:55:50 +0530

    final push
    final push

--- skillsquare/src/Components/Footer/Footer.jsx
+++ skillsquare/src/Components/Footer/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
             <p>
               <FaPhoneAlt className="footer-icon" />
               <a href="tel:+919876543210" className="phone-link">
-                +91 81900 16662
+                +91 8190016662
               </a>
             </p>
             <p>
